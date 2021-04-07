e48a8e46607e16c25edd5e5c393666e75d0ae923
Shashank sharma <78837537+theshashankk@users.noreply.github.com>
2021-04-07 14:56:56 +0530

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -22,7 +22,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('🔎 Finding the song...')
+    m = message.reply(' Finding the song...😎')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -51,7 +51,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by @Infinity_BOTs**'
+        rep = '**✅ Uploaded by [The Shashank Music Bot](http://t.me/The_ShashankMusicBot)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
